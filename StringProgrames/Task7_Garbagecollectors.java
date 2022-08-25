5411cbf93e1b6a62b7b74a1882b9ea4b8c4a963e
nileshmohale123 <111973528+nileshmohale123@users.noreply.github.com>
2022-08-25 07:55:32 +0530

    String programe added

---
+++ StringProgrames/Task7_Garbagecollectors.java
@@ -0,0 +1,22 @@
+package com.yash.java.stringprogrames;
+
+public class Task7_Garbagecollectors {
+
+	public void finalize() {
+		System.out.println("object is garbage collected");
+	}
+
+	public static void main(String args[]) {
+		// unreferance object
+		Task7_Garbagecollectors s1 = new Task7_Garbagecollectors();
+		Task7_Garbagecollectors s2 = new Task7_Garbagecollectors();
+		s1 = null;
+		s2 = null;
+		System.gc();
+		// now the first object referred by s3 is available for garbage collection
+		//refenrace object
+		Task7_Garbagecollectors s3 = new Task7_Garbagecollectors();
+		Task7_Garbagecollectors s4 = new Task7_Garbagecollectors();
+		s3 = s4;
+	}
+}
